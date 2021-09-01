eb558736819a254347cf43e32f1db3005d0873de
spences10 <spences10apps@gmail.com>
2021-09-01 20:18:22 +0100

    feat: :sparkles: add not found page
    config adapter Netlify, remove adapter static

---
+++ src/routes/__error.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+  export function load({ error, status }) {
+    return {
+      props: { error, status },
+    }
+  }
+</script>
+
+<script>
+  export let error, status
+</script>
+
+<svelte:head>
+  <title>{status}</title>
+</svelte:head>
+
+<h1 class="text-4xl mb-8">Uh oh!</h1>
+
+<p class="text-xl mb-8">
+  It looks like you've hit a page that doesn't exist!
+</p>
+
+<h2 class="text-3xl mb-8">{status}: {error.message}</h2>
